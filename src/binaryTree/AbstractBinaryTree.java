a968ef4ae5b3e91d7ce1fabf24221e77494fe466
Rinoyt <Sasha1Sim5@gmail.com>
2025-02-16 00:38:22 +0300

    New constructs for binary trees

--- src/binaryTree/AbstractBinaryTree.java
+++ src/binaryTree/AbstractBinaryTree.java
@@ -2,12 +2,26 @@ package binaryTree;
 
 import binaryTree.avlTree.AvlNode;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 public abstract class AbstractBinaryTree implements BinaryTree {
     protected Node root = null;
 
+    protected AbstractBinaryTree() {
+    }
+
     protected AbstractBinaryTree(Collection<Integer> values) {
+        for (Integer val : values) {
+            this.insert(val);
+        }
+    }
+
+    protected AbstractBinaryTree(Integer[] values) {
+        this(Arrays.asList(values));
+    }
+
+    protected AbstractBinaryTree(int[] values) {
         for (Integer val : values) {
             this.insert(val);
         }
@@ -24,11 +38,13 @@ public abstract class AbstractBinaryTree implements BinaryTree {
 
     @Override
     public int height() {
+        if (root == null) return 0;
         return root.getHeight();
     }
 
     @Override
     public int size() {
+        if (root == null) return 0;
         return root.getSize();
     }
 
